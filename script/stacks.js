351b731a6c456527d83ed9287be667618f7d7ccf
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-11-20 20:44:32 -0300

    :sparkles: Modificado a seções das tecnologias
    - Adicionado um link e um título para cada tecnologia
    
    - Adicionado um hover animado que altera a escala do elemento fazendo
      ficar maior

--- script/stacks.js
+++ script/stacks.js
@@ -2,53 +2,78 @@ const areaStacks = window.document.querySelector("#stacks");
 
 const stacks = [
 	{
+		name: "html",
 		src: "./assets/icons/icon-html.svg",
-		alt: "icon html"
+		alt: "icon html",
+		href: "https://developer.mozilla.org/pt-BR/docs/Web/HTML"
 	},
 	{
+		name: "css",
 		src: "./assets/icons/icon-css.svg",
-		alt: "icon css"
+		alt: "icon css",
+		href: "https://developer.mozilla.org/pt-BR/docs/Web/CSS"
 	},
 	{
+		name: "javascript",
 		src: "./assets/icons/icon-javascript.svg",
-		alt: "icon javascript"
+		alt: "icon javascript",
+		href: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
 	},
 	{
+		name: "typescript",
 		src: "./assets/icons/icon-typescript.svg",
-		alt: "icon typescript"
+		alt: "icon typescript",
+		href: "https://www.typescriptlang.org"
 	},
 	{
+		name: "react.js",
 		src: "./assets/icons/icon-react.svg",
-		alt: "icon react js"
+		alt: "icon react js",
+		href: "https://pt-br.reactjs.org"
 	},
 	{
+		name: "vue.js",
 		src: "./assets/icons/icon-vue.svg",
-		alt: "icon vue js"
+		alt: "icon vue js",
+		href: "https://vuejs.org"
 	},
 	{
+		name: "electron.js",
 		src: "./assets/icons/icon-electron.svg",
-		alt: "icon electron js"
+		alt: "icon electron js",
+		href: "https://www.electronjs.org"
 	},
 	{
+		name: "webpack.js",
 		src: "./assets/icons/icon-webpack.svg",
-		alt: "icon webpack js"
+		alt: "icon webpack js",
+		href: "https://webpack.js.org"
 	},
 	{
+		name: "gulp.js",
 		src: "./assets/icons/icon-gulp.svg",
-		alt: "icon gulp js"
+		alt: "icon gulp js",
+		href: "https://gulpjs.com"
 	},
 ];
 
 stacks.forEach(stack => {
-	const { src, alt } = stack;
+	const { 
+		src, 
+		alt, 
+		href, 
+		name 
+	} = stack;
 
 	areaStacks.innerHTML += `
-		<figure>
-			<img 
-				class="stack-image"
-				src="${ src }" 
-				alt="${ alt }"
-			/>
+		<figure title="${ name }">
+			<a href="${ href }" target="_blank">
+				<img 
+					class="stack-image"
+					src="${ src }" 
+					alt="${ alt }"
+				/>
+			</a
 		</figure>
 	`
 })
